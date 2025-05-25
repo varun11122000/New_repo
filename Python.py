67b7fd9205eba5dfeadb034cc7e62afe5bd2ca77
varun11122000 <varunj.ae18@gmail.com>
2025-05-25 20:32:34 +0530

    added new df

--- Python.py
+++ Python.py
@@ -2,6 +2,10 @@ import pandas as pd
 df=pd.DataFrame()
 print(df)
 columns=['A','B','V']
+column1=columns[::-1]
 width=[1,2,3,4]
 df=df[columns]
 print(df)
+df1=df.copy()
+df2=df1.copy()
+df2=df2[column1]
